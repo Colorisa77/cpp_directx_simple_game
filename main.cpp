1dcc5238a7542abe87e49aa621b3e8be199cd104
Colorisa77 <nikcola777@gmail.com>
2023-11-13 11:21:41 +1000

    creating main function

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include <windows.h>
+
+#define WINDOW_WIDTH = 640
+#define WINDOW_HEIGHT = 480
+
+
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE prevInstance,
+                   LPSTR lpstr, int nCmdLine) {
+
+    return 0;
+}
