b4276b419fe0493fa59d4bc51edcd8d220ad3c02
Colorisa77 <nikcola777@gmail.com>
2023-11-13 13:50:33 +1000

    making first window

--- main.cpp
+++ main.cpp
@@ -1,11 +1,19 @@
-#include <windows.h>
-
-#define WINDOW_WIDTH = 640
-#define WINDOW_HEIGHT = 480
+#include "p_window.h"
+const int WINDOW_WIDTH = 640;
+const int WINDOW_HEIGHT = 480;
 
 
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE prevInstance,
                    LPSTR lpstr, int nCmdLine) {
 
-    return 0;
+    window::Window wnd(WINDOW_WIDTH, WINDOW_HEIGHT, "test window");
+
+    MSG msg = {0};
+
+    while(GetMessage(&msg, NULL, 0, 0)) {
+        TranslateMessage(&msg);
+        DispatchMessage(&msg);
+    }
+
+    return (int)msg.wParam;
 }
